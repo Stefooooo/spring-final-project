cc22b5f5eb1219a9bb2bd26a361666d427a21b6c
Stefan Panaytov <StefoPanayotov@gmail.com>
2025-02-04 16:15:05 +0200

    a start of a journey

--- src/main/java/com/example/spring_final_project/User/model/User.java
+++ src/main/java/com/example/spring_final_project/User/model/User.java
@@ -38,11 +38,50 @@ public class User {
         appointments = new HashSet<>();
     }
 
-    public User(String firstName, String lastName, int age, boolean isActive, Set<Appointment> appointments) {
+    public User(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.isActive = isActive;
+        this.isActive = true;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Set<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(Set<Appointment> appointments) {
         this.appointments = appointments;
     }
 }
--- src/main/java/com/example/spring_final_project/User/repository/UserRepository.java
+++ src/main/java/com/example/spring_final_project/User/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.spring_final_project.User.repository;
 
-import com.example.spring_final_project.Doctor.model.User;
+import com.example.spring_final_project.User.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
